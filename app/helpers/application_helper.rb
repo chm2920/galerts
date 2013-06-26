254aad92e3990ee1705eb2b2ef933871a95b7713
pathfinder <chm2920@gmail.com>
2013-06-26 23:36:56 +0800

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,70 +31,60 @@ module ApplicationHelper
   PHRASE_UNSAFE = /[#{Regexp.quote aspecial}#{control}]/n
   TOKEN_UNSAFE  = /[#{Regexp.quote tspecial}#{control}#{sp}]/n
   
-  def mail_info(mail)
+  def mail_info(mail)    
+    require "base64"
+    
     header_part, body_part = mail.lstrip.split(/#{CRLF}#{CRLF}|#{CRLF}#{WSP}*#{CRLF}(?!#{WSP})/m, 2)
     
-    parts = mail.split('Content-Type: text/html')
-    if parts.length > 2
-      content = parts[1]
-      hsh = {}
-      
-      blocks = content.split('ebeff9')
-      0.upto (blocks.length - 1) do |i|
-        block = blocks[i]
-        block_t = ''
-        block_c = []
-        
-        block.scan(/<u><\/u><b>(.*?)<\/b><u><\/u>/m) do |t|
-          block_t = t[0].to_s
-        end        
-        
-        block.scan(/<td style(.*?)<\/td>/m) do |t|
-          hsh_block_c = {}
-          msg = t[0].to_s
-          hsh_block_c["msg"] = msg
-          msg.scan(/1111cc" href="(.*?)"(.*?)>(.*?)<\/a>/) do |link, b, title|
-            puts title
-            hsh_block_c["link"] = link[0].to_s
-            hsh_block_c["title"] = title[0].to_s
-          end
-          
-          msg.scan(/777777">(.*?)</) do |source|
-            hsh_block_c["source"] = source[0].to_s
-          end
-          
-          hsh_block_c["summary"] = msg.split('<br>')[2]
-          
-          block_c << hsh_block_c
-        end
-        
-        hsh[block_t] = block_c
-      end
-      arr = []
-      hsh.each do |k, v|
-        arr << "<b>" + k + "</b><br />"
-        v.each do |b|
-          arr << "msg--" + b["msg"].gsub('<', '&lt;').gsub('>', '&gt;') + "<br />" if b["msg"]
-          arr << "title--" + b["title"] + "<br />" if b["title"]
-          arr << "link--" + b["link"] + "<br />" if b["link"]
-          arr << "source--" + b["source"] + "<br />" if b["source"]
-          arr << "summary--" + b["summary"] + "<br />" if b["summary"]
-          arr << "<br />"
-        end
-      end
-      arr.join('').html_safe
-      #hsh.to_json
-    else
-      
-      # require 'mail'
-      # puts 'a'
-      # obj_mail = Mail.new(mail.to_s)
-      # puts 'b'
-      # # RubyVer.decode_base64(body_part)
-      # # body_part.unpack( 'm' ).first.force_encoding('ASCII_8BIT')
-      # obj_mail.methods.sort.join("<br />").html_safe
-      mail
+    boundary = ""
+    mail.scan(/boundary=(.*?) --/) do |b|
+      boundary = b[0].to_s
     end
+    
+    content = body_part.split('--' + boundary)[1]
+    content = content.split('base64')[1]
+    content = Base64.decode64(content)
+    content = content.split('This once a day')[0]
+         
+    info = {}
+    info["result"] = {}
+    content.scan(/new results for \[(.*?)\]/) do |a|
+      info["alert"] = a[0].to_s
+    end
+    
+    arr = content.split(/\n==/)
+    
+    0.upto (arr.length - 1) do |i|
+      section_t = ''
+      as = []
+      arr[i].scan(/= (.*?) -/) do |t|
+        section_t = t[0].to_s
+      end
+    
+      bs = arr[i].split("\r\n\r\n")
+    
+      1.upto (bs.length - 1) do |j|
+        obj = {}
+        bs[j].scan(/(.*?)\r\n(.*?)\r\n(.*?)\r\n<(.*?)>/m) do |a, b, c, d|
+          obj["title"] = a
+          if b.length > 40
+            obj["summary"] = b + "\r\n" + c
+          else
+            obj["source"] = b
+            obj["summary"] = c
+          end
+          obj["summary"] = obj["summary"].gsub("\r\n", " ")
+          obj["link"] = d.to_s
+          d.scan(/url\?sa=X&q=(.*?)&ct=ga/) do |link|
+            obj["link"] = link[0].to_s
+          end
+        end
+        as.push(obj)
+      end
+      info["result"][section_t] = as
+    end
+    
+    info.to_json
   end
   
 end
